d90d65e566ce46f159fa467d86534d43a8de7abd
Kristján Ingi Mikaelsson <kristjanmik@gmail.com>
2014-08-06 13:12:03 +0000

    initial rewrite

--- server.js
+++ server.js
@@ -1,10 +1,6 @@
-var express = require('express'),
-    app = module.exports = express(),
-    config = require('./config'),
-    fileModule = require('file'),
-    cache = require('./lib/cache'),
-    cors = require('./lib/cors'),
-    EE = require('events').EventEmitter;
+var express = require('express');
+var app = express();
+var cors = require('express-simple-cors');
 
 /**
  * Set the spacing to 0 for shorter output
@@ -12,43 +8,10 @@ var express = require('express'),
 app.set('json spaces', 0);
 
 /**
- * Create an event listener for app
- */
-EE.call(app);
-
-/*
- * Built in parser to acces the body values
- */
-app.use(express.bodyParser());
-
-/**
  * Cross-origin resource sharing
  */
 app.use(cors());
 
-/**
- * Caching layer
- */
-app.use(cache());
+require('apis-endpoints-middleware')(app);
 
-/**
- * Set up endpoints
- */
-fileModule.walkSync('./endpoints', function iterateEndpoints(dirPath, dirs, endpoints) {
-    if (endpoints && dirPath.indexOf('/test') < 0) endpoints.forEach(requireEndpoint);
-
-    function requireEndpoint(endpoint) {
-        if (endpoint.indexOf('.DS_Store') === -1) require('./' + dirPath + '/' + endpoint);
-    }
-});
-
-/**
- * Start the server
- */
-app.listen(config.port, function () {
-    app.emit('ready');
-});
-
-app.on('ready', function () {
-    if (!config.testing) console.log('Server running at port: ' + config.port);
-});
+app.listen(3000);
